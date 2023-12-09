faf66cbc0c250466d023429c9bfc9888ee88a8d4
Aathish R Viswam <aathishrv@gmail.com>
2023-12-09 13:04:32 +0530

    Update RegisterController.php

--- Margg/app/Http/Controllers/RegisterController.php
+++ Margg/app/Http/Controllers/RegisterController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 use Illuminate\Http\Request;
 
@@ -12,5 +14,21 @@ class RegisterController extends Controller
     public function index()
     {
         return view('home.register');
+    }
+
+    public function register(Request $request){
+        $request->validate(['name'=> 'required']);
+        $request->validate(['email'=> 'required']);
+        $request->validate(['phn_num'=>'required']);
+        $request->validate(['password'=> 'required|confirmed']);
+        $request->validate(['password_confirmation'=> 'required']);
+
+        $newuser = User::create([
+            'name'=> $request->input('name'),
+            'email'=> $request->input('email'),
+            'password'=> Hash::make($request->input('password')),
+            'phn_num'=>$request->input('phn_num'),
+        ]);
+
     }
 }
