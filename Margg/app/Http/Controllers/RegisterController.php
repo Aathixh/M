f3605312b20458dd4325a6aee1d964bf0e02c9c9
Aathish R Viswam <aathishrv@gmail.com>
2023-12-09 13:46:23 +0530

    controller edits

--- Margg/app/Http/Controllers/RegisterController.php
+++ Margg/app/Http/Controllers/RegisterController.php
@@ -16,19 +16,22 @@ class RegisterController extends Controller
         return view('home.register');
     }
 
-    public function register(Request $request){
-        $request->validate(['name'=> 'required']);
-        $request->validate(['email'=> 'required']);
-        $request->validate(['phn_num'=>'required']);
-        $request->validate(['password'=> 'required|confirmed']);
-        $request->validate(['password_confirmation'=> 'required']);
+    public function register(Request $request)
+    {
+        $request->validate(['name' => 'required']);
+        $request->validate(['email' => 'required']);
+        $request->validate(['phn_num' => 'required']);
+        $request->validate(['password' => 'required|confirmed']);
+        $request->validate(['password_confirmation' => 'required']);
 
         $newuser = User::create([
-            'name'=> $request->input('name'),
-            'email'=> $request->input('email'),
-            'password'=> Hash::make($request->input('password')),
-            'phn_num'=>$request->input('phn_num'),
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password')),
+            'phn_num' => $request->input('phn_num'),
         ]);
 
+        Auth::login($newuser);
+        // return redirect('/home');
     }
 }
--- Margg/resources/views/home/index.blade.php
+++ Margg/resources/views/home/index.blade.php
@@ -11,13 +11,15 @@
     <header>
         <h1>Welcome to Margg!</h1>
     </header>
-    <a href="/register"><button>Register</button></a>
-    <a href="/login"><button>Login</button></a>
+    <a href="/login"><button>Register</button></a>
+    <!-- <a href="/login"><button>Login</button></a> -->
     <footer>
         <h3>About</h3>
-        <p>"MARGG" is a startup idea focused on revolutionizing the way people track buses and enhance public transportation systems. Here's a brief overview of what MARGG could offer:
+        <p>"MARGG" is a startup idea focused on revolutionizing the way people track buses and enhance public
+            transportation systems. Here's a brief overview of what MARGG could offer:
 
-            Concept: MARGG is a comprehensive bus tracking and management system designed to provide real-time information to both passengers and transportation authorities.</p>
+            Concept: MARGG is a comprehensive bus tracking and management system designed to provide real-time
+            information to both passengers and transportation authorities.</p>
     </footer>
 </body>
 
