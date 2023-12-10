9844f64a4e41a5d91d2133a4c627a16f5df0e933
Aathish R Viswam <aathishrv@gmail.com>
2023-12-11 00:48:40 +0530

    added map and some of its functions

---
+++ Margg/public/js/map.js
@@ -0,0 +1,30 @@
+// Creating map options
+var mapOptions = {
+    center: [17.385044, 78.486671],
+    zoom: 10
+ }
+ // Creating a map object
+ var map = new L.map('map', mapOptions);
+ 
+ // Creating a Layer object
+ var layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+ 
+ // Adding layer to the map
+ map.addLayer(layer);
+
+ var scale = L.control.scale(); // Creating scale control
+
+ scale.addTo(map); // Adding scale control to the map
+ 
+ // Creating a Marker properties for dragging and moving
+ var markerOptions = {
+    title: "MyLocation",
+    clickable: true,
+    draggable: true
+ }
+ 
+ // Creating a marker
+ var marker = L.marker([17.385044, 78.486671], markerOptions);
+ 
+ // Adding marker to the map
+ marker.addTo(map);
