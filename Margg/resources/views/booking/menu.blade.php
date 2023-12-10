78550e753ec10e915476374e160d06feecddbaf6
Aathish R Viswam <aathishrv@gmail.com>
2023-12-10 13:52:54 +0530

    booking pg connections
    created locateController
    added some redirection to locate page
    created route and location blade

--- Margg/resources/views/booking/menu.blade.php
+++ Margg/resources/views/booking/menu.blade.php
@@ -13,6 +13,12 @@
             <div class="text">Logout</div>
         </button>
     </a>
+    <!-- here this /locate url is given to the book now button for the tym being... in real program it must redirect somewhere else like payment gateway.... -->
+    <a href="/locate"><button>
+            <div class="text">Book Now</div>
+        </button>
+    </a>
+
 </body>
 
 </html>
--- Margg/routes/web.php
+++ Margg/routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Http\Controllers\LocationController;
 use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\LocateController;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,3 +30,5 @@ Route::view('/home', 'home.index')->middleware('guest');
 Route::get('/signin', [LoginController::class, 'index']);
 Route::post('/authenticate', [LoginController::class, 'authenticate']);
 Route::get('/logout', [LoginController::class, 'logout']);
+
+Route::get('/locate', [LocateController::class, 'index']);
